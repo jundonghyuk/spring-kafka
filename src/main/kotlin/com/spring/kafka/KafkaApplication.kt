f50c6810c29af0dde7cc7405300f5a5642d8856e
JunDonghyuk <ehdgur4814@naver.com>
2025-03-01 14:16:49 +0900

    Initial commit.

---
+++ src/main/kotlin/com/spring/kafka/KafkaApplication.kt
@@ -0,0 +1,11 @@
+package com.spring.kafka
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KafkaApplication
+
+fun main(args: Array<String>) {
+	runApplication<KafkaApplication>(*args)
+}
