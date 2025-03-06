723fba7863505a4bd7fae17af17104c403b287da
JunDonghyuk <ehdgur4814@naver.com>
2025-03-06 22:50:38 +0900

    feat: 쉘 파일 내용 추가

--- command.sh
+++ command.sh
@@ -7,9 +7,21 @@
 # 프로듀서 클라이언트 실행
 ./kafka-console-producer --bootstrap-server kafka2:9092 --topic test2
 
+# 컨슈머 클라이언트 실행 (컨슈머 그룹 이름, 오프셋 리셋 옵션)
+./kafka-console-consumer --bootstrap-server kafka2:9092 --topic test4 --group consumer-group-1 --consumer-property auto.offset.reset=latest
+
 # 세그먼트 파일 확인
 # ${topic}-${partitionId} 형식으로 파일이 생성됨
 ./kafka-dump-log --print-data-log --files /var/lib/kafka/data/test-0/00000000000000000000.log
+
+# 특정 토픽에 대한 파티션 별 오프셋 정보
+./kafka-run-class kafka.tools.GetOffsetShell --broker-list kafka3:9093 --topic test4 --time -1
+
+# 컨슈머 그룹이름 확인
+./kafka-consumer-groups --bootstrap-server kafka2:9092 --list
+
+# 컨슈머 그룹의 특정 파티션에 대한 오프셋 정보
+./kafka-consumer-groups --bootstrap-server kafka2:9092 --describe --group test4
 
 # offset checkpoint 파일 확인
 cat replication-offset-checkpoint
